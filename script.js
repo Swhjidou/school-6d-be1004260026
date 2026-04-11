f9ea816566cc7eed6b6e53429aaf9adb4c193d88
Swhjidou <elbousidiahmed@gmail.com>
2026-04-11 01:40:24 +0000

    New st

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 <script>
-    let images = document.querySelectorAll('.galerie img');
+    window.onload = function() {
+        images = document.querySelectorAll('.galerie img');
+    };
+
     let currentIndex = 0;
 
-    function openModal(Index) {
+    function openModal(index) {
         currentIndex = index;
         document.getElementById("modal").style.display = "flex";
         updateModal()
@@ -16,9 +19,10 @@
         currentIndex += n;
         if (currentIndex >= images.length) currentIndex = 0;
         if (currentIndex < 0) currentIndex = images.length - 1;
+        updateModal()
     }
 
     function updateModal() {
-        document.getElementById("modal-img").src = Image[currentIndex].src;
+        document.getElementById("modal-img").src = Images[currentIndex].src;
     }
 </script>
